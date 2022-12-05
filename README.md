7c658411cfacc884cc7876be5ad48b19f0f940b5
Elijah Conners <business@elijahpepe.com>
2022-12-04 20:21:32 -0700

    chore: initial commit
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+# Cerberus
+
+CLI utility for Twitter to take the most recent direct message and validate it for being a likely spam message.
+
+## Methodology
+
+This utility checks the most recently received direct message and checks the username against a known RegEx expression for spam accounts. If it matches, the direct message will be deleted.
+
+## Installation
+
+Cerberus can be ran directly through the `cli.py` file in `/cerberus`. [Tweepy](https://www.tweepy.org) is a required dependency. Cerberus takes four arguments. The command for checking the most recent direct message can be seen below:
+
+```
+python cerberus/cli.py check --consumer-key <consumer key> --consumer-secret <consumer secret> --access-token <access token> --access-token-secret <access token secret>
+```
+
+The four arguments can obtained through the [Twitter Developer panel](https://developer.twitter.com). The access token and access token secret must be created with access to direct messages, and for the tool to delete direct messages, the project it's in needs to have Elevated access.
---
+++ cerberus/cli.py
@@ -0,0 +1,74 @@
+import argparse
+import re
+
+import tweepy
+import logging
+
+__version__ = '0.0.1'
+
+def check_username(username):
+	# Prone to change, temporary RegEx check. Very simple, yet effective
+	return bool(re.match(r'.*\d{8}', username))
+
+class Cerberus:
+	def __init__(self):
+		self.user = dict(
+			consumer_key='',
+			consumer_secret='',
+			access_token='',
+			access_token_secret=''
+		)
+
+	def check(self, args):
+		try:
+			# We don't check for the other keys for brevity
+			if args.consumer_key:
+				self.user['consumer_key'] = args.consumer_key
+				self.user['consumer_secret'] = args.consumer_secret
+				self.user['access_token'] = args.access_token
+				self.user['access_token_secret'] = args.access_token_secret
+				self.client = tweepy.Client(
+					consumer_key=self.user['consumer_key'],
+					consumer_secret=self.user['consumer_secret'],
+					access_token=self.user['access_token'],
+					access_token_secret=self.user['access_token_secret']
+				)
+				self.auth = tweepy.OAuth1UserHandler(
+					self.user['consumer_key'],
+					self.user['consumer_secret'],
+					self.user['access_token'],
+					self.user['access_token_secret']
+				)
+				self.api = tweepy.API(self.auth)
+		except Exception as e:
+			logging.exception(e)
+
+		direct_message = self.client.get_direct_message_events(expansions=['sender_id'])
+		username = direct_message.includes['users'][0].username
+		id = direct_message.data[0].id
+		if check_username(username):
+			self.api.delete_direct_message(id)
+
+def main():
+	parser = argparse.ArgumentParser(description=f'Cerberus v{__version__}')
+
+	subparsers = parser.add_subparsers(title='Commands', dest='subparser_name', metavar='<command>')
+	check_parser = subparsers.add_parser('check', help='check your DMs')
+
+	check_parser.add_argument('--consumer-key', dest='consumer_key', action='store', metavar='<consumer key>')
+	check_parser.add_argument('--consumer-secret', dest='consumer_secret', action='store', metavar='<consumer secret>')
+	check_parser.add_argument('--access-token', dest='access_token', action='store', metavar='<access token>')
+	check_parser.add_argument('--access-token-secret', dest='access_token_secret', action='store', metavar='<access token secret>')
+
+	args, _ = parser.parse_known_args()
+
+	cli = Cerberus()
+
+	try:
+		if args.subparser_name == 'check':
+			cli.check(args)
+	except KeyboardInterrupt:
+		logging.info('Keyboard interrupted!')
+
+if __name__ == '__main__':
+	main()
